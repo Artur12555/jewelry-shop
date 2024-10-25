e97dcd3fd5f4a8f3e838da94e4103c802660a660
X <jurektajne11@gmail.com>
2024-10-25 05:21:30 +0200

    working on categories and displaying product details pages

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import styled from 'styled-components';
+import { useTranslations } from 'next-intl';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -15,44 +16,67 @@ if (!supabaseUrl || !supabaseKey) {
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+const CarouselTitle = styled.h1`
+    font-family: 'Playfair Display', serif;
+    font-size: 32px;
+    margin-bottom: 4px;
+    color: purple;
+    font-style: italic;
+
+`;
+
+const CarouselSubTitle = styled.p`
+    font-family: 'Lato', sans-serif;
+    font-size: 16px;
+    margin-bottom: 40px;
+    color: black;
+
+`;
+
+const OuterCarouselContainer = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
 const CarouselContainer = styled.div`
   width: 80%;
-  margin: 40px auto 0; /* Set top margin for desktop */
-  border-radius: 120px; /* Add rounded corners */
-  overflow: hidden; /* Ensure the corners are applied */
-
+  margin: 0 auto;
+  margin-bottom: 10px;
+  border-radius: 120px;
+  margin-top: 40px;
+  overflow: hidden;
   @media (max-width: 768px) {
-    width: 100%; /* Full width on smaller screens */
-    border-radius: 0; /* Remove rounded corners */
-    margin-top: 0px; /* Set top margin for mobile */
+    width: 100%;
+    border-radius: 0;
+    margin-top: 0px;
   }
 `;
 
 const CarouselImage = styled.img`
   width: 100%;
   height: auto;
-  cursor: pointer; /* Change cursor to pointer to indicate it's clickable */
+  cursor: pointer;
 `;
 
 const BadgeContainer = styled.div`
-  width: 50%; /* Same width as the carousel */
-  margin: 20px auto; /* Space above and below the badge container */
-  display: flex; /* Use flexbox for layout */
-  justify-content: space-between; /* Evenly space the badges */
-  align-items: center; /* Center badges vertically */
+  width: 50%;
+  margin: 20px auto;
 `;
 
 const Badge = styled.div`
-  text-align: center; /* Center text below each SVG */
-    font-family: 'Lato', sans-serif;
-
+  text-align: center;
+  font-family: 'Lato', sans-serif;
 `;
 
 const BadgeImage = styled.img`
-  width: 90px; /* Adjust size as needed */
-  height: 90px; /* Adjust size as needed */
+  width: 90px;
+  height: 90px;
   margin: 0 auto;
-  margin-bottom: 5px; /* Space between icon and text */
+  margin-bottom: 5px;
+    @media (max-width: 768px) {
+    width: 150px;
+    height: 150px;
+  }
 `;
 
 interface Photo {
@@ -61,46 +85,45 @@ interface Photo {
 }
 
 const Home = () => {
+  const t = useTranslations('Badges');
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [showArrows, setShowArrows] = useState<boolean>(true);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPhotos = async () => {
       const { data, error } = await supabase
         .from('photos')
-        .select('url, site'); // Fetch both url and site
+        .select('url, site');
 
       if (error) {
         console.error('Error fetching photos:', error);
       } else {
-        setPhotos(data); // Set the state with the fetched data
-        console.log(data); // Debug: log fetched data
+        setPhotos(data);
+        console.log(data);
       }
     };
 
     fetchPhotos();
 
     const handleResize = () => {
-      if (window.innerWidth < 768) { // Adjust this value as needed
-        setShowArrows(false);
-      } else {
-        setShowArrows(true);
-      }
+      setShowArrows(window.innerWidth >= 768);
+      setIsMobile(window.innerWidth < 768);
     };
 
-    handleResize(); // Set initial value
-    window.addEventListener('resize', handleResize); // Add resize event listener
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize); // Cleanup on unmount
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   const handleClick = (site: string) => {
-    window.open(site, '_blank'); // Open the URL in a new tab
+    window.open(site, '_blank');
   };
 
-  const responsive = {
+  const responsiveImages = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 4,
@@ -111,16 +134,28 @@ const Home = () => {
     },
   };
 
+  const responsiveBadges = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <>
+    <OuterCarouselContainer>
       <CarouselContainer>
         <Carousel 
-          responsive={responsive}
+          responsive={responsiveImages}
           infinite={true}
-          arrows={showArrows} // Set arrows based on state
+          arrows={showArrows}
           renderArrowsWhenDisabled={false}
-          autoPlay={true} // Enable auto-swipe
-          autoPlaySpeed={5000} // Set swipe interval to 5 seconds
+          autoPlay={true}
+          autoPlaySpeed={5000}
         >
           {photos.map((photo, index) => (
             <div key={index} onClick={() => handleClick(photo.site)}>
@@ -129,28 +164,39 @@ const Home = () => {
           ))}
         </Carousel>
       </CarouselContainer>
-      
+      <CarouselTitle>In your style</CarouselTitle>
+      <CarouselSubTitle>biżuteria, która wyrazi kim jesteś</CarouselSubTitle>
+      </OuterCarouselContainer>
       <BadgeContainer>
-        <Badge>
-          <BadgeImage src="/images/fastdelivery.svg" alt="Fast delivery" />
-          Fast delivery
-        </Badge>
-        <Badge>
-          <BadgeImage src="/images/certification.svg" alt="Certifications" />
-          Certifications
-        </Badge>
-        <Badge>
-          <BadgeImage src="/images/satisfaction.svg" alt="Guaranteed satisfaction" />
-          Guaranteed satisfaction
-        </Badge>
-        <Badge>
-          <BadgeImage src="/images/uniqueness.svg" alt="Uniqueness" />
-          Uniqueness
-        </Badge>
-        <Badge>
-          <BadgeImage src="/images/payment.svg" alt="Safe payments" />
-          Safe payments
-        </Badge>
+        <Carousel 
+          responsive={responsiveBadges}
+          infinite={true}
+          autoPlay={isMobile} // Automatically play only if on mobile
+          autoPlaySpeed={3000}
+          arrows={false}
+          showDots={false}
+        >
+          <Badge>
+            <BadgeImage src="/images/fastdelivery.svg" alt="Fast delivery" />
+            <span>{t('fastDelivery')}</span>
+          </Badge>
+          <Badge>
+            <BadgeImage src="/images/certification.svg" alt="Certifications" />
+            <span>{t('certifications')}</span>
+          </Badge>
+          <Badge>
+            <BadgeImage src="/images/satisfaction.svg" alt="Guaranteed satisfaction" />
+            <span>{t('guaranteedSatisfaction')}</span>
+          </Badge>
+          <Badge>
+            <BadgeImage src="/images/uniqueness.svg" alt="Uniqueness" />
+            <span>{t('uniqueness')}</span>
+          </Badge>
+          <Badge>
+            <BadgeImage src="/images/payment.svg" alt="Safe payments" />
+            <span>{t('safePayments')}</span>
+          </Badge>
+        </Carousel>
       </BadgeContainer>
     </>
   );
